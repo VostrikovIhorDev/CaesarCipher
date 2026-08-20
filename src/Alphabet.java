6b6530ee68c2e3e8f5adfe7dfc2c36b7c023ba38
Ihor Vostrikov <vostrikov.ihor.dev@gmail.com>
2026-08-20 20:51:51 +0200

    Implement Alphabet class

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -1,23 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Provides access to the alphabet used by the Caesar cipher.
  */
 public class Alphabet {
 
     private static final String ALPHABET = "абвгдежзийклмнопрстуфхцчшщъыьэя.,«»\"':!? ";
+    private static final Map<Character, Integer> ALPHABET_MAP = getMap();
 
-    /**
-     * Returns the index of specified character
-     */
-    public static int getIndex(char letter) {
-        int index = ALPHABET.indexOf(letter);
-        if (index == -1) {
-            throw new IllegalArgumentException("Alphabet doesn't contain this letter");
+
+    private static Map<Character, Integer> getMap() {
+        Map<Character, Integer> alphabet = new HashMap<>();
+        for (int i = 0; i < ALPHABET.length(); i++) {
+            alphabet.put(ALPHABET.charAt(i), i);
         }
-        return index;
+        return alphabet;
     }
 
     /**
-     * Returns the character at the specified index
+     * Returns the index of specified character.
+     */
+    public static int getIndex(char letter) {
+        return ALPHABET_MAP.getOrDefault(letter, -1);
+    }
+
+    /**
+     * Returns the character at the specified index.
      */
     public static char getLetter(int index) {
         if (ALPHABET.length() - 1 < index || index < 0) {
