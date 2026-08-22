7e5ba01f3f7995adc39b07a46447b79a5c22b4c7
Ihor Vostrikov <vostrikov.ihor.dev@gmail.com>
2026-08-22 03:41:39 +0200

    Implement Main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        CaesarCipher a = new CaesarCipher();
-        System.out.println( a.encrypt("а", 3));
-        System.out.println( a.encrypt("я", 3));
-        System.out.println(a.  encrypt("привет", 3));
-        System.out.println(a.encrypt("A", 3));
-        System.out.println(a.decrypt(a.encrypt("привет", 3), 3));
+        FileManager fileManager = new FileManager(new CaesarCipher());
+        Validator validator = new Validator();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter path of input file");
+            String inputPath = scanner.nextLine();
+            System.out.println("Enter path of output file");
+            String outputPath = scanner.nextLine();
+            System.out.println("enter the key");
+            int key = scanner.nextInt();
+            if (!validator.isValidInputFile(inputPath)) {
+                System.out.println("Wrong input path");
+                return;
+            }
+            if (!validator.isValidOutputFile(outputPath)) {
+                System.out.println("Wrong output path");
+                return;
+            }
+            if (!validator.isValidKey(key)) {
+                System.out.println("Wrong key");
+                return;
+            }
 
+            System.out.println("Choose 1 for encrypt or 2 for decrypt the text");
+            int mode = scanner.nextInt();
 
+            try {
+                switch (mode) {
+                    case 1 -> {
+                        fileManager.encryptFile(inputPath, outputPath, key);
+                    }
+                    case 2 -> {
+                        fileManager.decryptFile(inputPath, outputPath, key);
+                    }
+                    default -> System.out.println("Wrong mode");
+                }
+            } catch (IOException e) {
+                System.out.println("Error while working with file " + e.getMessage());
+            }
+        }
     }
-    // большая буква , сдвиг
 }
